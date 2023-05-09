50710ab8e14694b1245fd64dfc02f8dc0a1b43eb
julianmedwards <julian.michael.edwards@gmail.com>
2023-05-09 14:05:03 -0500

    Got production working with docker but auth still broken in prod.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,4 @@ export default defineConfig({
     plugins: [react()],
     base: './',
     root: './packages/client',
-    // build: {
-    //     outDir: '../../dist/packages/client',
-    // },
 })

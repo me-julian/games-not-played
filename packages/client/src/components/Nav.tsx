228bbdc69b80a59d2606a2a5cb6e7690cf681c00
julianmedwards <julian.michael.edwards@gmail.com>
2023-05-11 13:32:07 -0500

    Basic react router rendering.

--- packages/client/src/components/Nav.tsx
+++ packages/client/src/components/Nav.tsx
@@ -1,12 +1,19 @@
+import { Link } from 'react-router-dom'
+
 function Nav() {
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand me-auto" href="/">
+                    <Link className="navbar-brand me-auto" to={'/'}>
                         Homepage
+                    </Link>
+                    <Link to={'about'} className="navbar-text">
+                        About
+                    </Link>
+                    <a className="navbar-text mx-4" href="/login">
+                        Login
                     </a>
-                    <a className="navbar-text mx-4">Login</a>
                 </div>
             </nav>
         </>
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './routes/Root'
+import About from './routes/About'
+import ErrorPage from './ErrorPage'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: '/about',
+                element: <About />,
+            },
+        ],
+    },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 )

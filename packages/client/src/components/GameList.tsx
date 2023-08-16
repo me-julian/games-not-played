8eeb195aac5f63ecbe5c64e1183f143c01afccb9
julianmedwards <julian.michael.edwards@gmail.com>
2023-08-16 17:55:22 +0000

    Render playing entries in separate section.

--- packages/client/src/components/GameList.tsx
+++ packages/client/src/components/GameList.tsx
@@ -7,10 +7,35 @@ type Props = {
 }
 
 function GameList({ entries }: Props) {
+    const categorizedEntries = entries?.reduce<{
+        playing: Client.Entry[]
+        other: Client.Entry[]
+    }>(
+        (entries, currentEntry) => {
+            if (currentEntry.isPlaying) entries.playing.push(currentEntry)
+            else entries.other.push(currentEntry)
+
+            return entries
+        },
+        { playing: [], other: [] }
+    )
+
     return (
         <>
-            {entries &&
-                entries.map((entry) => <Entry key={entry.id} entry={entry} />)}
+            {categorizedEntries && (
+                <>
+                    {categorizedEntries.playing.length > 0 &&
+                        categorizedEntries.playing.map((entry) => (
+                            <Entry key={entry.id} entry={entry} />
+                        ))}
+                    {categorizedEntries.playing.length > 0 && <hr />}
+
+                    {categorizedEntries.other.length > 0 &&
+                        categorizedEntries.other.map((entry) => (
+                            <Entry key={entry.id} entry={entry} />
+                        ))}
+                </>
+            )}
             <AddGame />
         </>
     )

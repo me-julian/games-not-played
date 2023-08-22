bcf7bac123d9d5e7091c36e6a11c5e17e9ec329e
julianmedwards <julian.michael.edwards@gmail.com>
2023-08-22 21:05:20 +0000

    Return searches from actual RAWG api.

--- packages/server/routes/api/search.ts
+++ packages/server/routes/api/search.ts
@@ -1,20 +1,63 @@
 import express from 'express'
 import { Router } from 'express'
 
+// Not exhaustive of everything actually sent by RAWG.
+type RawgGame = {
+    id: number
+    name: string
+    background_image: string
+    playtime: number
+    updated: string
+    saturated_color: string
+    dominant_color: string
+}
+
+type RawgSearchResults = {
+    count: number
+    next: string | null
+    previous: string | null
+    results: RawgGame[]
+}
+
+async function requestRawgGameSearch(url: string) {
+    const response = await fetch(url, {
+        method: 'get',
+    })
+    if (response.ok) {
+        return (await response.json()) as RawgSearchResults
+    } else {
+        throw new Error(response.status.toString())
+    }
+}
+
 const router: Router = express.Router()
 
 router.get('/search', async (req, res) => {
     const query = req.query.search
+    const page = req.query.page
 
-    const rawgRes = await fetch(
-        'http://localhost:5000/mockrawg/games?key=search=' + query,
-        {
-            method: 'get',
+    const baseUrl = `${process.env.RAWG_URL}/games?key=${process.env.RAWG_API_TOKEN}`
+    const dynamicQueries = `&page=${
+        page ? page : 1
+    }&page_size=10&search=${query}`
+
+    try {
+        const responseData = await requestRawgGameSearch(
+            baseUrl + dynamicQueries
+        )
+
+        if (responseData.count === 0) {
+            res.send(404)
         }
-    )
 
-    if (rawgRes.ok) {
-        res.send(await rawgRes.json())
+        res.send(responseData.results.slice(0, 10))
+    } catch (error) {
+        console.error(
+            `\nError: RAWG request to ${
+                baseUrl + dynamicQueries
+            } returned status code ${error}\n`
+        )
+        res.sendStatus(500)
     }
 })
 

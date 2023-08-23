98e99974348204238f0592c51b22c0227ceafcdd
julianmedwards <julian.michael.edwards@gmail.com>
2023-08-23 20:03:22 +0000

    Test seeding uses real data from RAWG

--- packages/server/db/seeders/data/testData.js
+++ packages/server/db/seeders/data/testData.js
@@ -52,44 +52,119 @@ exports.getEncryptedUsers = async () => {
 }
 exports.games = [
     {
-        id: 1,
-        name: 'Europa Universalis IV',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    },
-    {
-        id: 2,
-        name: 'Crusader Kings 2',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    },
-    {
-        id: 3,
-        name: 'The Elder Scrolls III: Morrowind',
-        playtime: 44,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    },
-    {
-        id: 4,
-        name: 'Stronghold: Crusader HD',
-        playtime: 15,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    },
-    {
-        id: 5,
+        id: 492,
         name: 'Yakuza 0',
-        playtime: 31,
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        playtime: 8,
+        backgroundImage:
+            'https://media.rawg.io/media/games/ca1/ca16da30f86d8f4d36261de45fb35430.jpg',
+        createdAt: '2023-08-23 19:34:02',
+        updatedAt: '2023-08-23 19:34:02',
+    },
+    {
+        id: 624,
+        name: 'FTL: Faster Than Light',
+        playtime: 6,
+        backgroundImage:
+            'https://media.rawg.io/media/games/5f4/5f4780690dbf04900cbac5f05b9305f3.jpg',
+        createdAt: '2023-08-23 19:33:38',
+        updatedAt: '2023-08-23 19:33:38',
+    },
+    {
+        id: 5538,
+        name: 'Dark Souls',
+        playtime: 48,
+        backgroundImage:
+            'https://media.rawg.io/media/games/582/582b5518a52f5086d15dde128264b94d.jpg',
+        createdAt: '2023-08-23 14:50:09',
+        updatedAt: '2023-08-23 14:50:09',
+    },
+    {
+        id: 10037,
+        name: 'Europa Universalis IV',
+        playtime: 6,
+        backgroundImage:
+            'https://media.rawg.io/media/games/eff/eff7b309ae98c32b48f22a10e7523106.jpg',
+        createdAt: '2023-08-23 19:32:51',
+        updatedAt: '2023-08-23 19:32:51',
+    },
+    {
+        id: 10340,
+        name: 'Crusader Kings II',
+        playtime: 3,
+        backgroundImage:
+            'https://media.rawg.io/media/games/c22/c22d804ac753c72f2617b3708a625dec.jpg',
+        createdAt: '2023-08-23 19:33:25',
+        updatedAt: '2023-08-23 19:33:25',
+    },
+    {
+        id: 10926,
+        name: 'Factorio',
+        playtime: 19,
+        backgroundImage:
+            'https://media.rawg.io/media/games/7e4/7e4e22b76da131e9690d5757555093c2.jpg',
+        createdAt: '2023-08-23 19:34:17',
+        updatedAt: '2023-08-23 19:34:17',
+    },
+    {
+        id: 13820,
+        name: 'The Elder Scrolls III: Morrowind',
+        playtime: 2,
+        backgroundImage:
+            'https://media.rawg.io/media/games/ccf/ccf26f6e3d553a04f0033a8107a521b8.jpg',
+        createdAt: '2023-08-23 19:33:50',
+        updatedAt: '2023-08-23 19:33:50',
+    },
+    {
+        id: 19418,
+        name: 'Europa Universalis: Rome - Gold Edition',
+        playtime: 1,
+        backgroundImage:
+            'https://media.rawg.io/media/screenshots/587/5870e2c822e51b950ea8da529e788b82.jpg',
+        createdAt: '2023-08-23 19:33:14',
+        updatedAt: '2023-08-23 19:33:14',
+    },
+    {
+        id: 31033,
+        name: 'Stronghold: Crusader',
+        playtime: null,
+        backgroundImage:
+            'https://media.rawg.io/media/games/3f1/3f16c94c5de75d27dcc25fac3cd43343.jpg',
+        createdAt: '2023-08-23 19:34:30',
+        updatedAt: '2023-08-23 19:34:30',
+    },
+    {
+        id: 37212,
+        name: 'Europa Universalis III',
+        playtime: 2,
+        backgroundImage:
+            'https://media.rawg.io/media/screenshots/bc9/bc9cf8d960c31553b8c16df8ce886c05.jpg',
+        createdAt: '2023-08-23 19:33:02',
+        updatedAt: '2023-08-23 19:33:02',
+    },
+    {
+        id: 257189,
+        name: 'StarCraft: Brood War',
+        playtime: null,
+        backgroundImage:
+            'https://media.rawg.io/media/screenshots/5d4/5d43fe0388f02f989d6afb0a9d50ad6c.jpg',
+        createdAt: '2023-08-23 19:35:36',
+        updatedAt: '2023-08-23 19:35:36',
+    },
+    {
+        id: 359139,
+        name: 'Shadow Tower Abyss',
+        playtime: null,
+        backgroundImage:
+            'https://media.rawg.io/media/games/d95/d95f4a4fb48f0afb488ec19cffa13bf1.jpg',
+        createdAt: '2023-08-23 19:34:54',
+        updatedAt: '2023-08-23 19:34:54',
     },
 ]
 exports.entries = [
     [
         // Julian's Entries
         {
-            gameId: 1,
+            gameId: 10037,
             order: 0,
             isStarred: false,
             isOwned: false,
@@ -99,7 +174,7 @@ exports.entries = [
             deletedAt: null,
         },
         {
-            gameId: 2,
+            gameId: 10340,
             order: 1,
             isStarred: false,
             isOwned: false,
@@ -109,7 +184,7 @@ exports.entries = [
             deletedAt: null,
         },
         {
-            gameId: 3,
+            gameId: 13820,
             order: 2,
             isStarred: false,
             isOwned: false,
@@ -122,7 +197,7 @@ exports.entries = [
     // Lodewijk's Entries
     [
         {
-            gameId: 1,
+            gameId: 10037,
             order: 0,
             isStarred: false,
             isOwned: false,
@@ -132,7 +207,7 @@ exports.entries = [
             deletedAt: null,
         },
         {
-            gameId: 2,
+            gameId: 10340,
             order: 1,
             isStarred: false,
             isOwned: false,
@@ -142,7 +217,7 @@ exports.entries = [
             deletedAt: null,
         },
         {
-            gameId: 3,
+            gameId: 13820,
             order: 2,
             isStarred: false,
             isOwned: false,
@@ -152,7 +227,7 @@ exports.entries = [
             deletedAt: null,
         },
         {
-            gameId: 4,
+            gameId: 31033,
             order: 3,
             isStarred: false,
             isOwned: false,
@@ -162,7 +237,7 @@ exports.entries = [
             deletedAt: null,
         },
         {
-            gameId: 5,
+            gameId: 492,
             order: 4,
             isStarred: false,
             isOwned: false,
@@ -175,7 +250,7 @@ exports.entries = [
     // Peter's Entries
     [
         {
-            gameId: 5,
+            gameId: 624,
             order: 0,
             isStarred: false,
             isOwned: false,
@@ -185,7 +260,7 @@ exports.entries = [
             deletedAt: null,
         },
         {
-            gameId: 3,
+            gameId: 257189,
             order: 1,
             isStarred: false,
             isOwned: false,

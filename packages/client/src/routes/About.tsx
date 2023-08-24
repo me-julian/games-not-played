609f908779ec9adc32727738e215a3d088097fe0
julianmedwards <julian.michael.edwards@gmail.com>
2023-08-24 21:16:44 +0000

    Slightly improved about page content & styling.

--- packages/client/src/routes/About.tsx
+++ packages/client/src/routes/About.tsx
@@ -1,3 +1,4 @@
+import '../public/about.css'
 import ActionNav from '../components/ActionNav'
 
 function About() {
@@ -5,23 +6,46 @@ function About() {
         <>
             <ActionNav actionName={'About'} />
             <main>
-                <p>
-                    Games Not Played is a web app intended to help users decide
-                    what game to play next.
-                </p>
-                <p>
-                    This is my final project for Inventive Academy's{' '}
-                    <a href="https://learn.inventiveacademy.io/p/full-stack-immersive-bootcamp-with-node">
-                        Full Stack Immersive
-                    </a>{' '}
-                    web development course.
-                </p>
-                <p>
-                    Want to see the source code for this app?{' '}
-                    <a href="https://github.com/julianmedwards/final-project-games-not-played">
-                        View the github repo.
-                    </a>
-                </p>
+                <section className="about-section">
+                    <h4>About the App</h4>
+                    <p>
+                        <span className="brand">Games Not Played</span> is a web
+                        app intended to help you decide what game to play next.
+                    </p>
+                    <p>
+                        The name is a reference to the poem{' '}
+                        <a href="https://www.poetryfoundation.org/poems/44272/the-road-not-taken">
+                            The Road Not Taken
+                        </a>{' '}
+                        by Robert Frost - in reference to the sometimes
+                        agonizing process of decision making.
+                    </p>
+                </section>
+                <section className="about-section">
+                    <h4>About Me</h4>
+                    <p>
+                        I'm Julian Edwards and I'm a web developer in Austin,
+                        Texas.
+                    </p>
+                    <p>
+                        This is my final project for Inventive Academy's{' '}
+                        <a href="https://learn.inventiveacademy.io/p/full-stack-immersive-bootcamp-with-node">
+                            Full Stack Immersive
+                        </a>{' '}
+                        web development course.
+                    </p>
+                </section>
+                <section className="about-section">
+                    <p>
+                        Wondering what this app was built with? Want to see the
+                        source code?
+                    </p>
+                    <p>
+                        <a href="https://github.com/julianmedwards/final-project-games-not-played">
+                            View the github repo.
+                        </a>
+                    </p>
+                </section>
             </main>
         </>
     )

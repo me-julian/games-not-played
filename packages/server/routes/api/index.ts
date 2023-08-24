510784767ac071d4eeb8d86f8435f7f8d270fe76
julianmedwards <julian.michael.edwards@gmail.com>
2023-08-24 16:46:16 +0000

    Removed unused import/router.

--- packages/server/routes/api/index.ts
+++ packages/server/routes/api/index.ts
@@ -2,11 +2,9 @@ import express from 'express'
 import { Router } from 'express'
 import userRouter from './user'
 import searchRouter from './search'
-import mockRawgRouter from './mockRawgApi'
 
 const router: Router = express.Router()
 router.use('/users', userRouter)
 router.use(searchRouter)
-router.use('/mockrawg', mockRawgRouter)
 
 export default router

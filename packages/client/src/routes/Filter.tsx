06d73a29dc554102a28be41d5fe6f6681217ab59
julianmedwards <julian.michael.edwards@gmail.com>
2023-08-29 20:07:28 +0000

    Reposition filter buttons, better border radiuses

--- packages/client/src/routes/Filter.tsx
+++ packages/client/src/routes/Filter.tsx
@@ -1,3 +1,4 @@
+import '../public/component-css/filter.css'
 import {
     Link,
     Navigate,
@@ -18,107 +19,108 @@ function Filter() {
         return (
             <>
                 <ActionNav actionName="Filter" />
-                <span className="toggle-btn">
-                    <input
-                        className="sr-only"
-                        type="radio"
-                        name="filter-by-custom-order"
-                        id="filter-by-custom-order"
-                        value={'Custom'}
-                        checked={currentFilter.state === 'custom'}
-                        onChange={() => currentFilter.setter('custom')}
-                    />
-                    <label htmlFor="filter-by-custom-order">Custom</label>
-                </span>
-                <span className="toggle-btn">
-                    <input
-                        className="sr-only"
-                        type="radio"
-                        name="filter-by-owned"
-                        id="filter-by-owned"
-                        value={'Owned'}
-                        checked={currentFilter.state === 'owned'}
-                        onChange={() => currentFilter.setter('owned')}
-                    />
-                    <label htmlFor="filter-by-owned">Owned</label>
-                </span>
-                <span className="toggle-btn">
-                    <input
-                        className="sr-only"
-                        type="radio"
-                        name="filter-by-length"
-                        id="filter-by-length"
-                        value={'Length'}
-                        checked={currentFilter.state === 'length'}
-                        onChange={() => currentFilter.setter('length')}
-                    />
-                    <label htmlFor="filter-by-length">Length</label>
-                    {currentFilter.state === 'length' && (
-                        <span className="toggle-btn swap">
-                            <input
-                                className="sr-only"
-                                type="checkbox"
-                                name="filter-order"
-                                id="filter-order"
-                                value={filterDirection.state}
-                                checked={
-                                    filterDirection.state === 'asc'
-                                        ? false
-                                        : true
-                                }
-                                onChange={() =>
-                                    filterDirection.setter(
+                <div id="filter-options">
+                    <span className="toggle-btn">
+                        <input
+                            className="sr-only"
+                            type="radio"
+                            name="filter-by-custom-order"
+                            id="filter-by-custom-order"
+                            value={'Custom'}
+                            checked={currentFilter.state === 'custom'}
+                            onChange={() => currentFilter.setter('custom')}
+                        />
+                        <label htmlFor="filter-by-custom-order">Custom</label>
+                    </span>
+                    <span className="toggle-btn">
+                        <input
+                            className="sr-only"
+                            type="radio"
+                            name="filter-by-owned"
+                            id="filter-by-owned"
+                            value={'Owned'}
+                            checked={currentFilter.state === 'owned'}
+                            onChange={() => currentFilter.setter('owned')}
+                        />
+                        <label htmlFor="filter-by-owned">Owned</label>
+                    </span>
+                    <span className="toggle-btn">
+                        <input
+                            className="sr-only"
+                            type="radio"
+                            name="filter-by-length"
+                            id="filter-by-length"
+                            value={'Length'}
+                            checked={currentFilter.state === 'length'}
+                            onChange={() => currentFilter.setter('length')}
+                        />
+                        <label htmlFor="filter-by-length">Length</label>
+                        {currentFilter.state === 'length' && (
+                            <span className="toggle-btn swap">
+                                <input
+                                    className="sr-only"
+                                    type="checkbox"
+                                    name="filter-order"
+                                    id="filter-order"
+                                    value={filterDirection.state}
+                                    checked={
                                         filterDirection.state === 'asc'
-                                            ? 'desc'
-                                            : 'asc'
-                                    )
-                                }
-                            />
-                            <label htmlFor="filter-order">
-                                {filterDirection.state.toUpperCase()}
-                            </label>
-                        </span>
-                    )}
-                </span>
-                <span className="toggle-btn">
-                    <input
-                        className="sr-only"
-                        type="radio"
-                        name="filter-by-date-added"
-                        id="filter-by-date-added"
-                        value={'Date Added'}
-                        checked={currentFilter.state === 'dateAdded'}
-                        onChange={() => currentFilter.setter('dateAdded')}
-                    />
-                    <label htmlFor="filter-by-date-added">Date Added</label>
-                    {currentFilter.state === 'dateAdded' && (
-                        <span className="toggle-btn swap">
-                            <input
-                                className="sr-only"
-                                type="checkbox"
-                                name="filter-order"
-                                id="filter-order"
-                                value={filterDirection.state}
-                                checked={
-                                    filterDirection.state === 'asc'
-                                        ? false
-                                        : true
-                                }
-                                onChange={() =>
-                                    filterDirection.setter(
+                                            ? false
+                                            : true
+                                    }
+                                    onChange={() =>
+                                        filterDirection.setter(
+                                            filterDirection.state === 'asc'
+                                                ? 'desc'
+                                                : 'asc'
+                                        )
+                                    }
+                                />
+                                <label htmlFor="filter-order">
+                                    {filterDirection.state.toUpperCase()}
+                                </label>
+                            </span>
+                        )}
+                    </span>
+                    <span className="toggle-btn">
+                        <input
+                            className="sr-only"
+                            type="radio"
+                            name="filter-by-date-added"
+                            id="filter-by-date-added"
+                            value={'Date Added'}
+                            checked={currentFilter.state === 'dateAdded'}
+                            onChange={() => currentFilter.setter('dateAdded')}
+                        />
+                        <label htmlFor="filter-by-date-added">Date Added</label>
+                        {currentFilter.state === 'dateAdded' && (
+                            <span className="toggle-btn swap">
+                                <input
+                                    className="sr-only"
+                                    type="checkbox"
+                                    name="filter-order"
+                                    id="filter-order"
+                                    value={filterDirection.state}
+                                    checked={
                                         filterDirection.state === 'asc'
-                                            ? 'desc'
-                                            : 'asc'
-                                    )
-                                }
-                            />
-                            <label htmlFor="filter-order">
-                                {filterDirection.state.toUpperCase()}
-                            </label>
-                        </span>
-                    )}
-                </span>
-
+                                            ? false
+                                            : true
+                                    }
+                                    onChange={() =>
+                                        filterDirection.setter(
+                                            filterDirection.state === 'asc'
+                                                ? 'desc'
+                                                : 'asc'
+                                        )
+                                    }
+                                />
+                                <label htmlFor="filter-order">
+                                    {filterDirection.state.toUpperCase()}
+                                </label>
+                            </span>
+                        )}
+                    </span>
+                </div>
                 <div className="fixed-action">
                     <Link className="button-wrapper" to={'/'}>
                         <button className="action-btn">Confirm</button>

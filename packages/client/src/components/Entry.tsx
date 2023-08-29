8a18753ddfd459855b27ce4547b7d8789269affd
julianmedwards <julian.michael.edwards@gmail.com>
2023-08-29 18:09:05 +0000

    Make grippy draggable handle & card info link.

--- packages/client/src/components/Entry.tsx
+++ packages/client/src/components/Entry.tsx
@@ -3,11 +3,7 @@ import { Link } from 'react-router-dom'
 import { Draggable } from '@hello-pangea/dnd'
 import { type Client } from '@games-not-played/types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-    faBars,
-    faGripLinesVertical,
-    faStar,
-} from '@fortawesome/free-solid-svg-icons'
+import { faBars, faStar } from '@fortawesome/free-solid-svg-icons'
 
 type Props = {
     entry: Client.Entry
@@ -22,58 +18,65 @@ function Entry({ index, entry }: Props) {
             index={index}
         >
             {(provided, snapshot) => (
-                <Link className="entry" to={`/details/${entry.id}`}>
-                    <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                    >
+                <div className="entry">
+                    <div ref={provided.innerRef} {...provided.draggableProps}>
                         <div className="wrapper">
                             <div
                                 className={`card ${
                                     snapshot.isDragging ? 'dragging' : ''
                                 }`}
                             >
-                                <div className="card-grip">
+                                <div
+                                    className="card-grip"
+                                    {...provided.dragHandleProps}
+                                >
                                     <FontAwesomeIcon icon={faBars} />
                                 </div>
-                                <div className="card-box">
-                                    <div className="name-and-star">
-                                        <h2 className="name text-wrap">
-                                            {entry.game.name}
-                                        </h2>
-                                        {entry.isStarred && (
-                                            <FontAwesomeIcon icon={faStar} />
-                                        )}
-                                    </div>
-                                    <div className="entry-info">
-                                        <div className="playtime-and-pills">
-                                            {entry.game.playtime && (
-                                                <span className="playtime">
-                                                    {entry.game.playtime} Hours
-                                                </span>
+                                <Link
+                                    className="card-link"
+                                    to={`/details/${entry.id}`}
+                                >
+                                    <div className="card-box">
+                                        <div className="name-and-star">
+                                            <h2 className="name text-wrap">
+                                                {entry.game.name}
+                                            </h2>
+                                            {entry.isStarred && (
+                                                <FontAwesomeIcon
+                                                    icon={faStar}
+                                                />
                                             )}
-                                            {entry.isOwned &&
-                                                !entry.isPlaying && (
-                                                    <div className="pill owned">
-                                                        Owned
-                                                    </div>
-                                                )}
                                         </div>
-                                        <div className="date-added">
-                                            Added on{' '}
-                                            {new Date(
-                                                entry.createdAt
-                                            ).toLocaleString(undefined, {
-                                                dateStyle: 'short',
-                                            })}
+                                        <div className="entry-info">
+                                            <div className="playtime-and-pills">
+                                                {entry.game.playtime && (
+                                                    <span className="playtime">
+                                                        {entry.game.playtime}{' '}
+                                                        Hours
+                                                    </span>
+                                                )}
+                                                {entry.isOwned &&
+                                                    !entry.isPlaying && (
+                                                        <div className="pill owned">
+                                                            Owned
+                                                        </div>
+                                                    )}
+                                            </div>
+                                            <div className="date-added">
+                                                Added on{' '}
+                                                {new Date(
+                                                    entry.createdAt
+                                                ).toLocaleString(undefined, {
+                                                    dateStyle: 'short',
+                                                })}
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
+                                </Link>
                             </div>
                         </div>
                     </div>
-                </Link>
+                </div>
             )}
         </Draggable>
     )

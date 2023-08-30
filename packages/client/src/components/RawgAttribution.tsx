cd94c07de56c6f3773ca7b6ed097ff13a473477d
julianmedwards <julian.michael.edwards@gmail.com>
2023-08-30 15:13:52 +0000

    Clearer RAWG spacing.

--- packages/client/src/components/RawgAttribution.tsx
+++ packages/client/src/components/RawgAttribution.tsx
@@ -1,9 +1,7 @@
 function RawgAttribution() {
     return (
         <div className="rawg-attribution">
-            <sub>
-                Data provided by <a href="https://rawg.io/">RAWG</a>
-            </sub>
+            Data provided by <a href="https://rawg.io/">RAWG</a>
         </div>
     )
 }

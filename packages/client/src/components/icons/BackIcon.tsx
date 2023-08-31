59f26b94579502fe4ed9920e38c51ef8d1a19201
julianmedwards <julian.michael.edwards@gmail.com>
2023-08-31 20:42:34 +0000

    Remove hardset svg colors.

--- packages/client/src/components/icons/BackIcon.tsx
+++ packages/client/src/components/icons/BackIcon.tsx
@@ -6,20 +6,18 @@ function BackIcon() {
             viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            style={{ verticalAlign: 'text-bottom' }}
+            className="icon"
         >
             <path
                 d="M19 12H5"
-                stroke="#FFFBFA"
-                stroke-opacity="0.9"
+                stroke="currentColor"
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
             />
             <path
                 d="M12 19L5 12L12 5"
-                stroke="#FFFBFA"
-                stroke-opacity="0.9"
+                stroke="currentColor"
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"

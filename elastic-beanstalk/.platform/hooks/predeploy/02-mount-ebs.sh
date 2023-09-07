40cfd1dd37916084b2e2f5bb0a7c6108a0702d79
julianmedwards <julian.michael.edwards@gmail.com>
2023-09-07 20:10:51 +0000

    Fix variable name.

--- elastic-beanstalk/.platform/hooks/predeploy/02-mount-ebs.sh
+++ elastic-beanstalk/.platform/hooks/predeploy/02-mount-ebs.sh
@@ -7,7 +7,7 @@ aws ec2 wait volume-in-use --region us-east-2 --volume-ids vol-0f08cb8b95f5138d0
 IMDS_TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 
 # Use the token to request this instance's id
-INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $AWS_METADATA_TOKEN" -s  http://169.254.169.254/latest/meta-data/instance-id)
+INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $IMDS_TOKEN" -s  http://169.254.169.254/latest/meta-data/instance-id)
 
 # Attach volume to this application instance on /dev/sdh
 aws ec2 attach-volume --volume-id vol-0f08cb8b95f5138d0 --instance-id $INSTANCE_ID --device /dev/sdh

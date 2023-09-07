a1352944ca4679858e41e3578471aa84be31ea74
julianmedwards <julian.michael.edwards@gmail.com>
2023-09-07 19:34:14 +0000

    Comment out modify_users pre-deploy script.

--- elastic-beanstalk/.platform/hooks/predeploy/01_modify_users.sh
+++ elastic-beanstalk/.platform/hooks/predeploy/01_modify_users.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Creating node user..."
-if ! id node &> /dev/null; then
-  useradd -u 1000 -g 1000 node
-fi
+# if ! id node &> /dev/null; then
+#   useradd -u 1000 -g 1000 node
+# fi
 
-echo "Adding node user to node group..."
-usermod -g 1000 node
+# echo "Adding node user to node group..."
+# usermod -g 1000 node

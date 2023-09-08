cd19a89ad16e11da9e79636b34c8c625e8b0310e
julianmedwards <julian.michael.edwards@gmail.com>
2023-09-08 18:18:02 +0000

    Replace api port when configuring nginx.

--- elastic-beanstalk/.platform/hooks/predeploy/04_configure_nginx.sh
+++ elastic-beanstalk/.platform/hooks/predeploy/04_configure_nginx.sh
@@ -7,6 +7,7 @@ NGINX_PLATFORM_DIR=".platform/nginx"
 NGINX_PLATFORM_CONF_DIR="$NGINX_PLATFORM_DIR/conf.d"
 
 # DOMAIN_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k DOMAIN_NAME)
+API_PORT=$(/opt/elasticbeanstalk/bin/get-config environment -k API_PORT)
 
 # Remove old HTTPS config
 
@@ -18,7 +19,8 @@ rm -f "$NGINX_CONF_DIR/https.conf"
 
 # echo "Configuring nginx to forward https://$DOMAIN_NAME to API server..."
 
-# sed -i "s/replaceme.com/$DOMAIN_NAME/g" "$NGINX_PLATFORM_CONF_DIR/games-not-played.conf"
+# sed -i "s/servernamereplaceme/$DOMAIN_NAME/g" "$NGINX_PLATFORM_CONF_DIR/games-not-played.conf"
+sed -i "s/apiportreplaceme/$API_PORT/g" "$NGINX_PLATFORM_CONF_DIR/games-not-played.conf"
 
 # Copy updated files
 

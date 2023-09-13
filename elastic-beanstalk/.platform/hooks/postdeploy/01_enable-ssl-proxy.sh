59ecb09bf99fa2414507639d79da093194d3919b
julianmedwards <julian.michael.edwards@gmail.com>
2023-09-13 21:25:53 +0000

    Fix missing semicolon and new snippet structure.

--- elastic-beanstalk/.platform/hooks/postdeploy/01_enable-ssl-proxy.sh
+++ elastic-beanstalk/.platform/hooks/postdeploy/01_enable-ssl-proxy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Add snippet for actual SSL protected proxy server
-echo "server {" >> "/etc/nginx/conf.d/games-not-played.conf"
-echo "    include /etc/nginx/ssl.conf" >> "/etc/nginx/conf.d/games-not-played.conf"
-echo "}" >> "/etc/nginx/conf.d/games-not-played.conf"
+echo "include /etc/nginx/snippets/ssl.conf;" >> "/etc/nginx/conf.d/games-not-played.conf"
 
 # Restart the server
 service nginx restart

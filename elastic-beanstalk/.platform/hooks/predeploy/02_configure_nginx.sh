68d3449d7593dd00bf0a878dff87f3652386d8b5
julianmedwards <julian.michael.edwards@gmail.com>
2023-09-13 21:39:58 +0000

    Copy snippets in configure_nginx script.

--- elastic-beanstalk/.platform/hooks/predeploy/02_configure_nginx.sh
+++ elastic-beanstalk/.platform/hooks/predeploy/02_configure_nginx.sh
@@ -20,11 +20,14 @@ rm -f "$NGINX_CONF_DIR/https.conf"
 echo "Configuring nginx to forward https://$API_DOMAIN to API server..."
 
 sed -i "s/DOMAINREPLACEME/$API_DOMAIN/g" "$NGINX_PLATFORM_CONF_DIR/games-not-played.conf"
-sed -i "s/APIPORTREPLACEME/$API_PORT/g" "$NGINX_PLATFORM_CONF_DIR/games-not-played.conf"
+sed -i "s/DOMAINREPLACEME/$API_DOMAIN/g" "$NGINX_DIR/snippets/ssl.conf"
+sed -i "s/APIPORTREPLACEME/$API_PORT/g" "$NGINX_DIR/games-not-played.conf"
+sed -i "s/APIPORTREPLACEME/$API_PORT/g" "$NGINX_PLATFORM_CONF_DIR/snippets/ssl.conf"
 
 # Copy updated files
 
 echo "Copying nginx configuration files..."
 
 yes | cp -rf "$NGINX_PLATFORM_DIR/nginx.conf" "$NGINX_DIR/nginx.conf"
+yes | cp -rf "$NGINX_PLATFORM_DIR/snippets/ssl.conf" "$NGINX_DIR/snippets/ssl.conf"
 yes | cp -rf "$NGINX_PLATFORM_CONF_DIR/games-not-played.conf" "$NGINX_CONF_DIR/games-not-played.conf"
--- elastic-beanstalk/.platform/hooks/predeploy/05-copy-snippets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -r -f /etc/nginx/snippets
-mkdir /etc/nginx/snippets
-cp /var/proxy/staging/nginx/snippets/ssl.conf /etc/nginx/snippets/ssl.conf

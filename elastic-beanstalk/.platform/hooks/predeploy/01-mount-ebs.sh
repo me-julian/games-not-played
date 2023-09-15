cd4a9a7c01bf8cba4b1f9cdbe2fcf1b413769fd5
julianmedwards <julian.michael.edwards@gmail.com>
2023-09-15 15:12:21 +0000

    Fix and improve hanging mount ebs script.

--- elastic-beanstalk/.platform/hooks/predeploy/01-mount-ebs.sh
+++ elastic-beanstalk/.platform/hooks/predeploy/01-mount-ebs.sh
@@ -9,32 +9,40 @@ INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $IMDS_TOKEN" -s  http://169.254
 DATA_VOLUME_ID=$(/opt/elasticbeanstalk/bin/get-config environment -k DATA_VOLUME_ID)
 
 # Check if volume is already attached.
+echo "Checking if '$DATA_VOLUME_ID' already attached to Instance '$INSTANCE_ID'..."
 VOLUME_ATTACHED=$(aws ec2 describe-volumes --filters Name=attachment.instance-id,Values=$INSTANCE_ID | jq --arg DATA_VOLUME_ID $DATA_VOLUME_ID 'contains({"Volumes": [{"VolumeId": $DATA_VOLUME_ID}]})')
-echo "Volume attached evaluated to $VOLUME_ATTACHED"
-if [ VOLUME_ATTACHED = "true" ]
+
+if [ $VOLUME_ATTACHED = "true" ]
 then
-    echo "Volume '$DATA_VOLUME_ID' already attached to Instance '$INSTANCE_ID'"
+    echo "Volume already attached."
 else
-    echo "Volume '$DATA_VOLUME_ID' not attached to current instance."
-    echo "Attempt to attach to self."
+    echo "Volume is not attached to current instance."
 
-    # Wait for the volume to be available
-    #   Currently can't handle if the volume is attached to an instance
-    #   other than the current application.
-    aws ec2 wait volume-in-use --region us-east-2 --volume-ids $DATA_VOLUME_ID
+    # Check that the volume isn't in use by another instance
+    echo "Checking if volume is being used by any other resources..."
+    NOT_IN_USE=$(aws ec2 describe-volumes --region us-east-2 --filter Name=volume-id,Values=$DATA_VOLUME_ID --query 'Volumes[*].{VolumeID:VolumeId,Attachments:Attachments}' | jq 'contains([{Attachments:[]}])')
 
-    # Attach volume to this application instance on /dev/sdh
-    aws ec2 attach-volume --volume-id $DATA_VOLUME_ID --instance-id $INSTANCE_ID --device /dev/sdh
+    if [ $NOT_IN_USE = "true" ]
+    then
+        # Attach volume to this application instance on /dev/sdh
+        echo "Attaching volume to self..."
+        aws ec2 attach-volume --volume-id $DATA_VOLUME_ID --instance-id $INSTANCE_ID --device /dev/sdh
+    else
+        echo "UNEXPECTED STATE OF VOLUME: Volume already in use by another instance!"
+        exit
+    fi
 fi
 
 # Create filesystem on volume if it's empty
+echo "Checking if volume contains filesystem..."
 if [ $(blkid | grep -c "/dev/sdh") -ne 0 ]
 then
     echo "Volume is empty, creating filesystem..."
     mkfs -t ext4 /dev/sdh
+else
+    echo "Existing filesystem detected on volume."
 fi
 
 # Mount EBS to folder for MySQL docker image
-echo "Mounting EBS device at /dev/sdh to /data/mysql if it isn't already..."
-mkdir -p /data/mysql
-mount /dev/sdh/mysql /data/mysql
+echo "Mounting EBS device at /dev/sdh to /data if it isn't already..."
+mount /dev/sdh /data

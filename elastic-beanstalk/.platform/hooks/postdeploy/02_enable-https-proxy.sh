63e78ea5b22623301932d1a497efe36699f26fbd
julianmedwards <julian.michael.edwards@gmail.com>
2023-09-15 17:15:02 +0000

    Add echos to enable https script.

--- elastic-beanstalk/.platform/hooks/postdeploy/02_enable-https-proxy.sh
+++ elastic-beanstalk/.platform/hooks/postdeploy/02_enable-https-proxy.sh
@@ -2,7 +2,9 @@
 
 # Start the actual https proxy server by
 # including the snippet in the app .conf
+echo "Enable https proxy server."
 echo "include /etc/nginx/snippets/https-proxy.conf;" >> "/etc/nginx/conf.d/games-not-played.conf"
 
 # Restart the server
+echo "Restart nginx with https server."
 service nginx restart

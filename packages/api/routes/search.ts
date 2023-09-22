3e371b9b9fb54bb6db657ba9086b0e993aa63a33
julianmedwards <julian.michael.edwards@gmail.com>
2023-09-22 00:42:37 +0000

    Add authentication req to search route.

--- packages/api/routes/search.ts
+++ packages/api/routes/search.ts
@@ -1,40 +1,45 @@
 import { RAWG } from '@games-not-played/types'
 import express from 'express'
 import { Router } from 'express'
+import passport from 'passport'
 import config from '../config'
 
 const router: Router = express.Router()
 
-router.get('/search', async (req, res) => {
-    const query = req.query.search
-    const page = req.query.page
+router.get(
+    '/search',
+    passport.authenticate('jwt', { session: false }),
+    async (req, res) => {
+        const query = req.query.search
+        const page = req.query.page
 
-    const pageSize = 10
+        const pageSize = 10
 
-    const urlSections = [
-        `${config.rawgUrl}/games?key=${config.rawgApiToken}`,
-        `&page=${page ? page : 1}&page_size=${pageSize}`,
-        `&search=${query}`,
-        '&search_precise=true',
-    ]
+        const urlSections = [
+            `${config.rawgUrl}/games?key=${config.rawgApiToken}`,
+            `&page=${page ? page : 1}&page_size=${pageSize}`,
+            `&search=${query}`,
+            '&search_precise=true',
+        ]
 
-    const url = urlSections.flat().join('')
+        const url = urlSections.flat().join('')
 
-    const response = await fetch(url, {
-        method: 'get',
-    })
+        const response = await fetch(url, {
+            method: 'get',
+        })
 
-    if (response.ok) {
-        const data = (await response.json()) as RAWG.SearchResults
-        res.send(data)
-    } else if (response.status === 404) {
-        res.sendStatus(404)
-    } else {
-        console.error(
-            `\nError: RAWG request to ${url} returned status code ${response.status}\n`
-        )
-        res.sendStatus(500)
+        if (response.ok) {
+            const data = (await response.json()) as RAWG.SearchResults
+            res.send(data)
+        } else if (response.status === 404) {
+            res.sendStatus(404)
+        } else {
+            console.error(
+                `\nError: RAWG request to ${url} returned status code ${response.status}\n`
+            )
+            res.sendStatus(500)
+        }
     }
-})
+)
 
 export default router
